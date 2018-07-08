664a2ec6e5615659b47ddd91bbb8f636f0295936
Bubba <pcman1115@gmail.com>
2018-07-08 15:31:15 -0400

    Client starting
    I now have the client starting.

---
+++ minecraft/io/guthub/ytboy/Client.java
@@ -0,0 +1,12 @@
+package io.guthub.ytboy;
+
+public class Client {
+	String NAME;
+	String VERSION;
+	public Client(String name, String version) {
+		this.NAME = name;
+		this.VERSION = version;
+		start();
+	}
+	public void start() {}
+}
---
+++ minecraft/io/guthub/ytboy/jupiter/Jupiter.java
@@ -0,0 +1,16 @@
+package io.guthub.ytboy.jupiter;
+
+import io.guthub.ytboy.Client;
+
+public class Jupiter extends Client{
+
+	public Jupiter() {
+		super("Jupiter", "0.1");
+	}
+	@Override
+	public void start() {
+		super.start();
+	}
+	
+
+}
